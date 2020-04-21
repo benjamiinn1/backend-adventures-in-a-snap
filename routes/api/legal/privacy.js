b7fc2bb46290f7529a9486345510f0eed269e3bb
benjamiinn1 <33909077+benjamiinn1@users.noreply.github.com>
2020-04-20 20:45:36 -0500

    added TOS

--- routes/api/legal/privacy.js
+++ routes/api/legal/privacy.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express();
 const legalTxt = require("./legalTxt");
 router.get("/privacy", (req, res) => {
-  console.log(legalTxt);
   res.send(`${legalTxt.PRIVACY()}`);
+});
+
+router.get("/tos", (req, res) => {
+  res.send(`${legalTxt.TOS()}`);
 });
 
 module.exports = router;
