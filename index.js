24894934ef0cb09e8a3c0141847e309cde994696
benjamiinn1 <33909077+benjamiinn1@users.noreply.github.com>
2020-04-20 22:42:27 -0500

    updated

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const webServer = require("./services/web-server.js");
 const database = require("./services/database.js");
+const path = require("path");
+const webServerConfig = require("./config/web-server");
+const express = require("express");
+const router = express();
+const legalAPI = require("./routes/api/legal/privacy");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const startup = async () => {
   console.log("Starting application");
@@ -16,7 +22,15 @@ const startup = async () => {
   try {
     console.log("Initializing web server module");
 
-    webServer.initialize();
+    router.get("/", async (_req, res) => {
+      res.sendFile(path.join(__dirname + "/index.html"));
+    });
+
+    router.use("/legal", legalAPI);
+
+    router.listen(webServerConfig.PORT).on("listening", () => {
+      console.log(`Web server listening on localhost:${webServerConfig.PORT}`);
+    });
   } catch (err) {
     console.error(err);
 
