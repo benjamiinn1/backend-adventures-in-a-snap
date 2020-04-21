5dffdb9fbdfa87bda489a216fcc3deada53f0164
benjamiinn1 <33909077+benjamiinn1@users.noreply.github.com>
2020-04-20 23:22:54 -0500

    added google auth

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const router = express();
 const legalAPI = require("./routes/api/legal/privacy");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const startup = async () => {
   console.log("Starting application");
@@ -20,11 +21,35 @@ const startup = async () => {
   }
 
   try {
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: keys.googleClientID,
+          clientSecret: keys.googleClientSecret,
+          callbackURL: "/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+          console.log(`access token: ${accessToken}`);
+          console.log(`refresh token: ${refreshToken}`);
+          console.log(`profile: ${JSON.stringify(profile)}`);
+        }
+      )
+    );
+
     console.log("Initializing web server module");
 
     router.get("/", async (_req, res) => {
       res.sendFile(path.join(__dirname + "/index.html"));
     });
+
+    router.get(
+      "/auth/google",
+      passport.authenticate("google", {
+        scope: ["profile", "email"],
+      })
+    );
+
+    router.get("/auth/google/callback", passport.authenticate("google"));
 
     router.use("/legal", legalAPI);
 
